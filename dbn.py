7a635a93ce8ec3d70758c1651158b4b880b1a6b8
zhangzuizui <421350889@qq.com>
2016-12-07 19:59:04 +0800

    Add files via upload

--- dbn.py
+++ dbn.py
@@ -12,12 +12,11 @@ dataset = datasets.fetch_mldata("MNIST Original")
 	dataset.data / 255.0, dataset.target.astype("int0"), test_size = 0.33)
 
 dbn = DBN(
-    #set the model
-    [trainX.shape[1], 400, 200, 100, 50, 25, 10],
+    [trainX.shape[1], 400, 200, 100, 50, 25, 10],   #模型层次
 	#[trainX.shape[1], 300, 10],
-	learn_rates = 0.1,
-	learn_rate_decays = 0.9,
-	epochs = 2,
+	learn_rates = 0.1,   #学习率
+	learn_rate_decays = 0.9,  #学习率递减率
+	epochs = 10,    #迭代次数
     verbose = 1)
 dbn.fit(trainX, trainY)
 
@@ -25,10 +24,9 @@ preds = dbn.predict(testX)
 print classification_report(testY, preds)
 
 for i in np.random.choice(np.arange(0, len(testY)), size = (10,)):
-	# classify the digit
 	pred = dbn.predict(np.atleast_2d(testX[i]))
  
 	image = (testX[i] * 255).reshape((28, 28)).astype("uint8")
 	print "Actual digit is {0}, predicted {1}".format(testY[i], pred[0])
 	cv2.imshow("Digit", image)
-	cv2.waitKey(0)
+	cv2.waitKey(0) 
